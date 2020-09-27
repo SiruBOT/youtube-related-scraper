869c0bd45594202fb2b3128529f319104747ec4c
Sannoob <cotwo0139@gmail.com>
2020-09-27 17:10:30 +0900

    feat: improve sorting, updated condition No IPs available

--- src/RoutePlanner.js
+++ src/RoutePlanner.js
@@ -7,9 +7,9 @@ class RoutePlanner {
    * @typedef {Number} Retry
    * @param {CIDRBlocks} ipBlocks
    * @param {Array<String>} excludeIps Array of exclude Ips
-   * @param {Retry} retry -1 = Default Value, 0 = Infinity
+   * @param {Retry} [retry = -1] -1 = Default Value, 0 = Infinity
    */
-  constructor (ipBlocks, excludeIps = [], retry) {
+  constructor (ipBlocks, excludeIps = [], retry = -1) {
     if (!Array.isArray(ipBlocks)) throw new Error('ipBlocks must be type Array')
     if (ipBlocks.length < 0) throw new Error('RoutePlanner must have at least one ipBlocks')
     if (!Array.isArray(excludeIps)) throw new Error('excludeIps must be type Array')
@@ -55,10 +55,10 @@ class RoutePlanner {
   }
 
   getRandom () {
-    const sorted = this.ipBlocks.sort(el => this.sort(el.broadcastAddress)).reverse()
+    const sorted = this.ipBlocks.sort((a, b) => this.sort(a, b))
     const randomResult = randomIP(sorted[0].broadcastAddress, sorted[0].subnetMasks)
     this.usedCount.set(sorted[0].broadcastAddress, this.usedCount.get(sorted[0].broadcastAddress) + 1)
-    if (this.failedAddresses.size >= 1 && this.failedAddresses.size === this.ipBlocks.map(el => el.size).reduce((pre, cur) => pre + cur)) throw new Error('No IPs available')
+    if (this.failedAddresses.size >= 1 && this.failedAddresses.size >= this.ipBlocks.map(el => el.size).reduce((pre, cur) => pre + cur)) throw new Error('No IPs available')
     if (this.excludeIps.includes(randomResult) || this.failedAddresses.get(randomResult)) return this.getRandom()
     return { ip: randomResult, family: ip.isV4Format(randomResult) ? 'ipv4' : 'ipv6' }
   }
@@ -66,9 +66,7 @@ class RoutePlanner {
   sort (a, b) {
     const countA = this.usedCount.get(a)
     const countB = this.usedCount.get(b)
-    if (countA > countB) return -1
-    if (countA < countB) return 1
-    return 0
+    return countA - countB
   }
 }
 module.exports = RoutePlanner
